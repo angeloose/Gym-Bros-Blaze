7fdf52fbd2f618ae87b1384bce5473d1cce06830
Angelo <angelocervanaa@gmail.com>
2025-03-22 21:49:43 -0700

    Merge back to main success kinda

--- gymBrosBlaze/app/index.tsx
+++ gymBrosBlaze/app/index.tsx
@@ -1,154 +1,19 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, Button } from "react-native";
-import { useRouter } from "expo-router";
-import WorkoutWeek from "../Components/WorkoutWeek"
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/homeScreen.js";
+import LoginScreen from "../screens/loginScreen.js";
+import SignUpScreen from "../screens/signUpScreen.js";
+import MainScreen from "../screens/mainScreen.tsx";
 
-export default function Index() {
-  const router = useRouter();
-  const userName = "JOHN JOHNATHAN";
+const Stack = createStackNavigator();
 
+export default function App() {
   return (
-    <View style={styles.container}>
-      {/* Top Section */}
-      <View style={styles.topContainer}>
-        <View style={styles.profileContainer}>
-          <Image
-            source={require("../assets/images/demo_pfp.jpg")}
-            style={styles.profilePic}
-          />
-          
-          <View style={styles.subprofileContainer}>
-            <Text style={styles.subGreeting}>WELCOME BACK,</Text>
-            <Text style={styles.greeting}>{userName}</Text>
-          </View>
-        </View>
-        {/* Manage Friends Button */}
-        <Button 
-          title="Manage Friends" 
-          onPress={() => router.push('/friends')} 
-          color="#6A5ACD"
-        />
-      </View>
-      
-      <View style={styles.scoreHeadingContainer}>
-        <Text style={styles.scoreHeading}>SCORE</Text>
-      </View>
-
-      {/* Score Row */}
-      <View style={styles.scoreContainer}>
-        <View style={styles.scoreBox}>
-          <Image
-            source={require("../assets/images/Consis_Icon.png")}
-            style={{ width: 40, height: 40, marginLeft: 1 }}
-          />
-          <Text style={styles.scoreValue}>90</Text>
-          <Text style={styles.scoreTitle}>CONSISTENCY</Text>
-        </View>
-        <View style={styles.scoreBox}>
-          <Image
-            source={require("../assets/images/pr_icon.png")}
-            style={{ width: 40, height: 40, marginRight: 1 }}
-          />
-          <Text style={styles.scoreValue}>10</Text>
-          <Text style={styles.scoreTitle}>PR</Text>
-        </View>
-        <View style={styles.scoreBox}>
-          <Image
-            source={require("../assets/images/overall_icon.png")}
-            style={{ width: 40, height: 40, marginLeft: 2 }}
-          />
-          <Text style={styles.scoreValue}>100</Text>
-          <Text style={styles.scoreTitle}>OVERALL</Text>
-        </View>
-      </View>
-
-      <View style={styles.scoreHeadingContainer}>
-        <Text style={styles.scoreHeading}>PAST WEEK</Text>
-      </View>
-
-      {/* Week View Section */}
-      <WorkoutWeek />
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Sign Up" component={SignUpScreen} />
+        <Stack.Screen name="Main" component={MainScreen} />
+      </Stack.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#292929",
-  },
-  topContainer: {
-    marginTop: 20,
-    marginLeft: 15,
-    alignItems: "flex-start",
-  },
-  profileContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  subprofileContainer: {
-    flexDirection: "column",
-    alignItems: "flex-start",
-    marginBottom: 10,
-  },
-  profilePic: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    marginRight: 10,
-  },
-  subGreeting: {
-    fontSize: 15,
-    fontWeight: "bold",
-    color: "#bdbdbd",
-  },
-  greeting: {
-    fontSize: 25,
-    fontWeight: "bold",
-    color: "#fff",
-  },
-  scoreHeadingContainer: {
-    alignItems: "flex-start",
-    marginTop: 30,
-    marginLeft: 20,
-  },
-  scoreHeading:{
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#fff",
-  },
-  scoreContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-    marginHorizontal: 15,
-    marginTop: 10,
-  },
-  scoreBox: {
-    backgroundColor: "#424242",
-    borderRadius: 10,
-    padding: 10,
-    width: "30%",
-    alignItems: "center",
-  },
-  scoreTitle: {
-    fontSize: 14,
-    color: "#bdbdbd",
-  },
-  scoreValue: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#fff",
-    marginTop: 5,
-  },
-  centerContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  welcomeText: {
-    fontSize: 18,
-    color: "#fff",
-    marginBottom: 20,
-  },
-});
---
+++ gymBrosBlaze/screens/mainScreen.tsx
@@ -0,0 +1,154 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, Button } from "react-native";
+import { useRouter } from "expo-router";
+import WorkoutWeek from "../Components/WorkoutWeek"
+
+export default function Index() {
+  const router = useRouter();
+  const userName = "JOHN JOHNATHAN";
+
+  return (
+    <View style={styles.container}>
+      {/* Top Section */}
+      <View style={styles.topContainer}>
+        <View style={styles.profileContainer}>
+          <Image
+            source={require("../assets/images/demo_pfp.jpg")}
+            style={styles.profilePic}
+          />
+          
+          <View style={styles.subprofileContainer}>
+            <Text style={styles.subGreeting}>WELCOME BACK,</Text>
+            <Text style={styles.greeting}>{userName}</Text>
+          </View>
+        </View>
+        {/* Manage Friends Button */}
+        <Button 
+          title="Manage Friends" 
+          onPress={() => router.push('/friends')} 
+          color="#6A5ACD"
+        />
+      </View>
+      
+      <View style={styles.scoreHeadingContainer}>
+        <Text style={styles.scoreHeading}>SCORE</Text>
+      </View>
+
+      {/* Score Row */}
+      <View style={styles.scoreContainer}>
+        <View style={styles.scoreBox}>
+          <Image
+            source={require("../assets/images/Consis_Icon.png")}
+            style={{ width: 40, height: 40, marginLeft: 1 }}
+          />
+          <Text style={styles.scoreValue}>90</Text>
+          <Text style={styles.scoreTitle}>CONSISTENCY</Text>
+        </View>
+        <View style={styles.scoreBox}>
+          <Image
+            source={require("../assets/images/pr_icon.png")}
+            style={{ width: 40, height: 40, marginRight: 1 }}
+          />
+          <Text style={styles.scoreValue}>10</Text>
+          <Text style={styles.scoreTitle}>PR</Text>
+        </View>
+        <View style={styles.scoreBox}>
+          <Image
+            source={require("../assets/images/overall_icon.png")}
+            style={{ width: 40, height: 40, marginLeft: 2 }}
+          />
+          <Text style={styles.scoreValue}>100</Text>
+          <Text style={styles.scoreTitle}>OVERALL</Text>
+        </View>
+      </View>
+
+      <View style={styles.scoreHeadingContainer}>
+        <Text style={styles.scoreHeading}>PAST WEEK</Text>
+      </View>
+
+      {/* Week View Section */}
+      <WorkoutWeek />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#292929",
+  },
+  topContainer: {
+    marginTop: 20,
+    marginLeft: 15,
+    alignItems: "flex-start",
+  },
+  profileContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  subprofileContainer: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    marginBottom: 10,
+  },
+  profilePic: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  subGreeting: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#bdbdbd",
+  },
+  greeting: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  scoreHeadingContainer: {
+    alignItems: "flex-start",
+    marginTop: 30,
+    marginLeft: 20,
+  },
+  scoreHeading:{
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  scoreContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginHorizontal: 15,
+    marginTop: 10,
+  },
+  scoreBox: {
+    backgroundColor: "#424242",
+    borderRadius: 10,
+    padding: 10,
+    width: "30%",
+    alignItems: "center",
+  },
+  scoreTitle: {
+    fontSize: 14,
+    color: "#bdbdbd",
+  },
+  scoreValue: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#fff",
+    marginTop: 5,
+  },
+  centerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  welcomeText: {
+    fontSize: 18,
+    color: "#fff",
+    marginBottom: 20,
+  },
+});
